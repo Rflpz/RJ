5b10395bb4e8aac00273024c01dd252438d79a3b
Rflpz <rafael.lopez@surespot.com>
2016-11-08 14:22:55 -0600

    Template and icon app

---
+++ RJ/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  RJ
+//
+//  Created by Rafael Lopez on 11/8/16.
+//  Copyright © 2016 Rflpz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
